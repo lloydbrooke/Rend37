dff4a02456af518bcf952412e557ef2ee0574903
Eman Tahir <83791618+emanntahirr@users.noreply.github.com>
2026-04-01 21:12:56 +0100

    remove communities, events and map routers, tests and templates
    these don't belong on the discussions branch

--- routers/discussion.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends, Request
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from typing import Dict, List
-import models
-from database import get_db
-import auth_utils
